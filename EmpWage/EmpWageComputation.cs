aa4648ab247638650bb2f23bd056ba0f6b710791
charasullia <charanrajr713@gmail.com>
2021-09-14 12:13:04 +0530

    [charansullia]added EmpTotalWage

--- EmpWage/EmpWageComputation.cs
+++ EmpWage/EmpWageComputation.cs
@@ -8,17 +8,22 @@ namespace EmpWage
     {
         public void EmpComputation()
         {
-            int IS_PRESENT = 1;
+            int IS_FULL_TIME_PRESENT = 1;
+            int empRatePerHr = 20;
+            int empHr = 0;
+            int totalWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-            if(empCheck==IS_PRESENT)
+            if(empCheck==IS_FULL_TIME_PRESENT)
             {
-                Console.WriteLine("employee is present");
+                empHr = 8;
             }
             else
             {
-                Console.WriteLine("employee is absent");
+                empHr = 0;
             }
+            totalWage = (empHr * empRatePerHr);
+            Console.WriteLine("total employee wage is:" + totalWage);
 
         }
         
