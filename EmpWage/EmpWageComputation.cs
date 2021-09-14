598019fc94cdfcdae0cc3bdf76d4107424671095
charasullia <charanrajr713@gmail.com>
2021-09-14 12:21:59 +0530

    [charansullia]added AddPartTimeWage

--- EmpWage/EmpWageComputation.cs
+++ EmpWage/EmpWageComputation.cs
@@ -9,14 +9,19 @@ namespace EmpWage
         public void EmpComputation()
         {
             int IS_FULL_TIME_PRESENT = 1;
+            int IS_PART_TIME_PRESENT = 2;
             int empRatePerHr = 20;
             int empHr = 0;
             int totalWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if(empCheck==IS_FULL_TIME_PRESENT)
             {
                 empHr = 8;
+            }
+            else if(empCheck==IS_PART_TIME_PRESENT)
+            {
+                empHr = 4;
             }
             else
             {
