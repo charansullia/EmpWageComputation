35269b827a239c8476087cb66d439e465c52fa13
charasullia <charanrajr713@gmail.com>
2021-09-14 13:46:24 +0530

    [charansullia]added EmpWage for month

--- EmpWage/EmpWageComputation.cs
+++ EmpWage/EmpWageComputation.cs
@@ -6,33 +6,38 @@ namespace EmpWage
 {
     class EmpWageComputation
     {
+        public const int IS_FULL_TIME_PRESENT = 1;
+        public const int IS_PART_TIME_PRESENT = 2;
+        public const int empRatePerHr = 20;
+        public const int WorkingDayPerMonth = 20;
         public void EmpComputation()
         {
-            int IS_FULL_TIME_PRESENT = 1;
-            int IS_PART_TIME_PRESENT = 2;
-            int empRatePerHr = 20;
             int empHr = 0;
-            int totalWage = 0;
+            int empWage = 0;
+            int totalempWage = 0;
+            for (int day = 0; day < WorkingDayPerMonth; day++)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
-                    case 1:
+                    case IS_FULL_TIME_PRESENT:
                         empHr = 8;
                         break;
 
-                    case 2:
+                    case IS_PART_TIME_PRESENT:
                         empHr = 4;
                         break;
                     default:
                         empHr = 0;
                         break;
                 }
-                totalWage = (empHr * empRatePerHr);
-                Console.WriteLine("total employee wage is:" + totalWage);
 
+                empWage = empRatePerHr * empHr;
+                Console.WriteLine("emp wage is:" + empWage);
             }
+                totalempWage += empWage;
+                Console.WriteLine("emp totalwage is:" + totalempWage);
 
         }
     }
